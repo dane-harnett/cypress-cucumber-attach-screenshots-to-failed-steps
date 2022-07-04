e5273f93e691aee9a9cd66fa3989d06981ea1518
Samuel Pampolini <samuelpampolini@gmail.com>
2022-07-04 16:57:40 +0100

    Change to check when the test are retrying to avoid undefined

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 afterEach(() => {
-  const screenshotsFolder = Cypress.config("screenshotsFolder");
-  if (window.cucumberJson?.generate) {
-    const testState = window.testState;
-    const stepResult =
-      testState.runTests[testState.currentScenario.name][testState.currentStep];
-    if (stepResult?.status === "failed") {
-      const screenshotFileName = `${testState.feature.name} -- ${testState.currentScenario.name} (failed).png`;
-      cy.readFile(
-        `${screenshotsFolder}/${Cypress.spec.name}/${screenshotFileName}`,
-        "base64"
-      ).then((imgData) => {
+  const currentTest = cy.state('test');
+  if (window.cucumberJson?.generate && currentTest.state === 'failed'
+      && currentTest.currentRetry() === currentTest.retries()) {
+    
+    const screenshotsFolder = Cypress.config('screenshotsFolder');
+    const { testState } = window;
+    const scenario = testState.runTests[testState.currentScenario.name];
+    const stepResult = scenario[testState.currentStep];
+
+    const scenarioName = testState.currentScenario.name.endsWith('.')
+        ? testState.currentScenario.name.substring(0, testState.currentScenario.name.length - 1)
+        : testState.currentScenario.name;
+
+    const screenshotFileName = `${testState.feature.name} -- ${scenarioName} (failed).png`;
+
+    cy.readFile(`${screenshotsFolder}/${Cypress.spec.name}/${screenshotFileName}`, 'base64').then((imgData) => {
+      if (imgData) {
         stepResult.attachment = {
+          name: screenshotFileName,
           data: imgData,
-          media: { type: "image/png" },
+          media: { type: 'image/png' },
           index: testState.currentStep,
           testCase: testState.formatTestCase(testState.currentScenario),
         };
-      });
-    }
+      }
+    });
   }
 });
